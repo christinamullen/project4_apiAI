bca428f4847122c47910c9756b796b6ef081ded0
Christina dt <cnicodc@gmail.com>
2023-05-14 09:32:21 -0700

    start end input listeners

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-function getContractors(){
-  const url = "";
 
-  //fetch the data
-  fetch(url)
-  .then((response) => {
-      if (!response.ok) {
-          throw new Error("Network response was not ok");
-      }
-      return response.json(); //Parses JSON data
-  })
+//fetch the data
+const startInput = document.querySelector('#startQuery');
+const endInput = document.querySelector('#endQuery');
+
+startInput.addEventListener('input', () => {
+    fetch(`https://basic-api-proxy-server.cnico078.repl.co/autocomplete?q=${input.value}`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json(); //Parses JSON data
+    })
   .then((data) => {
-    
+    console.log(data);
   })
   .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
   });
-}
+});
+
+endInput.addEventListener('input', () => {
+    fetch(`https://basic-api-proxy-server.cnico078.repl.co/autocomplete?q=${input.value}`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json(); //Parses JSON data
+    })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+  });
+});
+
